93a4b62c5b61abb6fec700c892e001d216d88393
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-14 23:18:17 -0700

    styling is hard

--- dona-client/src/components/SideNav.vue
+++ dona-client/src/components/SideNav.vue
@@ -1,9 +1,51 @@
 <template>
-  <div></div>
+  <div>
+    <div class="d-grid mt-4">
+      <button
+        v-for="TodoList in TodoLists"
+        :key="TodoList.name"
+        class="row btn btn-light btn-lg m-1"
+        type="button"
+      >
+        {{ TodoList.name }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SideNav',
+  data() {
+    return {
+      TodoLists: [
+        // {
+        //   name: String,
+        //   icon: String,
+        //   size: Number,
+        // },
+        {
+          name: 'New York Trip',
+          icon: '#',
+          size: 4,
+        },
+        {
+          name: 'Car Stuff',
+          icon: '*',
+          size: 2,
+        },
+        {
+          name: 'Work',
+          icon: '@',
+          size: 6,
+        },
+        {
+          name: 'Shopping',
+          icon: '!',
+          size: 1,
+        },
+      ],
+    };
+  },
 };
 </script>
--- dona-client/src/views/Home.vue
+++ dona-client/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <SideNav />
+  <div class="pe-4">
+    <div class="container-fluid m-3 mt-4" style="height: 95vh">
+      <div class="row h-100">
+        <div class="col-4 bg-white rounded-4 p-3 pe-4 ps-4">
+          <SideNav />
+        </div>
+        <div class="col-8">dlkjf</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import SideNav from '@/components/SideNav.vue';
 
 export default {
