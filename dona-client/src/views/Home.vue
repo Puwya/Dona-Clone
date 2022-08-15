f5c5f012b8ad68dbea0e43a5f27a0e9d4be71c6f
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-14 21:56:18 -0700

    setup

---
+++ dona-client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <SideNav />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SideNav from '@/components/SideNav.vue';
+
+export default {
+  name: 'Home-',
+  components: {
+    SideNav,
+  },
+};
+</script>
